ef7e83c8dca695157a236e4247daf1fef07b44b3
JshReaper <jshchansen@gmail.com>
2016-01-11 12:32:17 +0100

    some more stuff

---
+++ Assets/my scripts/GibOnTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour {
+    public GameObject gib;
+
+    void OnTriggerEnter()
+    {
+        Instantiate(gib, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
